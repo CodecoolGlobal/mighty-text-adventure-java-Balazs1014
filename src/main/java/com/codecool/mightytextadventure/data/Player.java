4509735c6668246b012cb61b5dd9f7ac3deb05f7
BalintGeri <109275872+BalintGeri@users.noreply.github.com>
2022-12-05 18:11:15 +0100

    deleted inventory class, fixed player class

--- src/main/java/com/codecool/mightytextadventure/data/Player.java
+++ src/main/java/com/codecool/mightytextadventure/data/Player.java
@@ -7,4 +7,21 @@ public class Player {
     public List<Items> inventory;
     public Area currentArea;
     public int points;
+
+    public Player(String name, Area currentArea) {
+        this.name = name;
+        this.currentArea = currentArea;
+
+        inventory.add(Items.LAPTOP);
+        inventory.add(Items.NOOBNESS);
+        points = 0;
+    }
+
+    public void removeFromInventory(Items item) {
+        inventory.remove(item);
+    }
+
+    public void addToInventory(Items item) {
+        inventory.add(item);
+    }
 }
